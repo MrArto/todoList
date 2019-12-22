b82c9aa18c624daa7879a641f38e2e61f6c34bc3
MrArto <artavazd_arto@yahoo.com>
2019-12-23 01:01:47 +0900

    Add files via upload

---
+++ precache-manifest.6cfde677a769784093b00e6c3167f292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7ca286a118415bfcd3d980461bb452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "195f44835bf80d4e1f7c",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "70475927e633b320533f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "195f44835bf80d4e1f7c",
+    "url": "/static/js/2.a3f8a2aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a3f8a2aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70475927e633b320533f",
+    "url": "/static/js/main.e7c3c470.chunk.js"
+  },
+  {
+    "revision": "c1943459750e13b04b22",
+    "url": "/static/js/runtime-main.bf76ec9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
